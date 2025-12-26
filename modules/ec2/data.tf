b10e71b9e944e67d9f99584af3d65b3e86487921
Shami2810 <shami.ssk2810@gmail.com>
2025-12-26 16:03:05 +0530

    learn terraform code

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,7 +1,3 @@
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
-
-output "sg" {
-  value = data.aws_security_group.selected
-}
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = var.ami
   instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "${var.name}-${var.env}"
@@ -13,4 +14,21 @@ resource "aws_route53_record" "record" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "ansible" {
+  depends_on = [aws_route53_record.record]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "sudo pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/shamidevsecops/roboshop-ansible roboshop.yml -e role_name=${var.name}"
+    ]
+  }
 }

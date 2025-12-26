df5e92594a414216c9b08d8ac438c92e5958a8cf
Shami2810 <shami.ssk2810@gmail.com>
2025-12-26 20:52:38 +0530

    learn terraform code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,13 +3,14 @@ resource "aws_instance" "instance" {
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
-  instance_market_options {
-    market_type = "spot"
+instance_market_options {
+  market_type = "spot"
 
-    spot_options {
-      instance_interruption_behavior = "stop"
-      spot_instance_type = "persistent"
-    }
+  spot_options {
+    instance_interruption_behavior = "stop"
+    spot_instance_type             = "persistent"
+  }
+}
 
   tags = {
     Name = "${var.name}-${var.env}"
